dec48ae408850289ee4fcbcd6895b4c4e17dd2de
Uneven Prankster <33995085+GithubPrankster@users.noreply.github.com>
2024-10-28 23:32:34 -0300

    Sobrecarga

--- CartaoDeCredito.java
+++ CartaoDeCredito.java
@@ -4,66 +4,59 @@ import java.util.Scanner;
 
 class CartaoDeCredito {
 	private String numero, nomeTitular, cpfTitular;
-	private float limite, saldo;
+	private float limite, cashback;
 	
-	private float get_limite() {
-		return limite;
+	public CartaoDeCredito(String num, String nome, String cpf) {
+		numero = num;
+		nomeTitular = nome;
+		cpfTitular = cpf;
+		limite = 1000.0f;
+		cashback = 5.0f / 100.0f;
 	}
 	
-	private void set_limite(float valor) {
-		limite = valor;
-	}
-	
-	private float get_saldo() {
-		return limite;
-	}
-	
-	private void set_saldo(float valor) {
-		saldo = valor;
-	}
-	
-	
-	public CartaoDeCredito(float l_inicial) {
-		limite = l_inicial;
-		set_saldo(get_limite());
-	}
-	
-	public float consultarSaldo() {
-		return get_saldo();
+	public CartaoDeCredito(String num, String nome, String cpf, float lim, float cash) {
+		numero = num;
+		nomeTitular = nome;
+		cpfTitular = cpf;
+		limite = lim;
+		cashback = cash / 100.0f;
 	}
 	
 	public float consultarLimite() {
-		return get_limite();
+		return limite;
 	}
 	
 	public boolean realizarTransação(float valor) {
-		float resultado = get_saldo() - valor;
+		float resultado = limite - valor;
 		if(resultado > 0.f) {
-			set_saldo(resultado);
+			limite = resultado;
 			return true;
 		}
 		return false;
 	}
 	
-	public void alterarLimite(float novo_limite) {
-		set_limite(novo_limite);
-		set_saldo(novo_limite);
+	public boolean realizarTransaçãoCashback(float valor) {
+		if(realizarTransação(valor)) {
+			limite += valor * cashback;
+			return true;
+		}
+		return false;
 	}
 }
 
 public class Principal {
 
 	public static void main(String[] args) {
-		CartaoDeCredito cartao = new CartaoDeCredito(1000.0f);
+		CartaoDeCredito cartao = new CartaoDeCredito("4002 8922 1234 1449", "Robson Ferreira", "000.800.169-21");
 		Scanner entrada = new Scanner(System.in);
 		
 		System.out.println("Seja bem-vindo! Aqui estão suas opções:");
 		
 		while(true) {
 			
-			System.out.println("1. Consultar Saldo");
-			System.out.println("2. Consultar Limite");
-			System.out.println("3. Realizar Transação");
+			System.out.println("1. Consultar Limite");
+			System.out.println("2. Realizar Transação");
+			System.out.println("3. Realizar Transação com Cashback");
 			System.out.println("4. Sair");
 			
 			int opção = entrada.nextInt();
@@ -74,21 +67,30 @@ public class Principal {
 			}else {
 				switch(opção) {
 				case 1:
-					System.out.println("Seu saldo é: " + cartao.consultarSaldo());
+					System.out.println("Seu saldo é: " + cartao.consultarLimite());
 					break;
 				case 2:
-					System.out.println("Seu limite é: " + cartao.consultarLimite());
-					break;
-				case 3:
 					System.out.println("Por favor comunique o valor da transação.");
 					float valor = entrada.nextFloat();
 					if(cartao.realizarTransação(valor)) {
-						System.out.println("Seu saldo agora é: " + cartao.consultarSaldo());
+						System.out.println("Seu limite agora é: " + cartao.consultarLimite());
 					}else {
 						System.out.println("Transação alta demais para seu saldo atual.");
 					}
 					break;
-				}
+				case 3:
+					System.out.println("Por favor comunique o valor da transação.");
+					float valor1 = entrada.nextFloat();
+					if(cartao.realizarTransaçãoCashback(valor1)) {
+						System.out.println("Seu saldo agora é: " + cartao.consultarLimite());
+					}else {
+						System.out.println("Transação alta demais para seu saldo atual.");
+					}
+					break;
+				default:
+					System.out.println("Opção desconhecida. Tente novamente.");
+					break;
+				}	
 			}
 		}
 		
